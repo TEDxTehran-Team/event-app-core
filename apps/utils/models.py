1dcf54bbb042481d3cab16b4b9f7675396490a75
Hasan Noori <xishma@gmail.com>
2020-01-30 16:31:46 +0330

    adds link model in utils

--- apps/utils/models.py
+++ apps/utils/models.py
@@ -25,3 +25,39 @@ class BaseModel(SafeDeleteModel):
 
     class Meta:
         abstract = True
+
+
+class Link(BaseModel):
+    title = models.CharField(
+        max_length=255,
+        verbose_name=_('title'),
+        help_text=_("an optional title describing the link."),
+        blank=True,
+        null=True
+    )
+    # todo do we need to make the roles hardcode, or a related model? I don't really feel good about this one!
+    role = models.SlugField(
+        max_length=31,
+        verbose_name=_('role'),
+        help_text=_(
+            'an optional slug, describing the link for other programs, such as applications.'),
+        blank=True,
+        null=True
+    )
+    url = models.URLField(
+        verbose_name=_('url'),
+        help_text=_('the url address for the link.')
+    )
+
+    class Meta:
+        verbose_name = _("link")
+        verbose_name_plural = _("links")
+        ordering = ['created_at']
+
+    def __str__(self):
+        if self.title:
+            return self.title
+        elif self.role:
+            return self.role
+        else:
+            return self.url[:63]+'...'
