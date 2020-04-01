3f503157c5afa8efff1dc79124b4e038cabbcead
Hasan Noori <xishma@gmail.com>
2020-04-01 13:22:21 +0430

    Adds basic admin for organizers

--- apps/organizers/admin.py
+++ apps/organizers/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Organizer, OrganizerAccount, AboutOrganizer
+
+
+class OrganizerAccountInline(admin.TabularInline):
+    model = OrganizerAccount
+    extra = 1
+    autocomplete_fields = ['user']
+
+
+class AboutOrganizerInline(admin.StackedInline):
+    model = AboutOrganizer
+    extra = 1
+
+
+class OrganizerAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = [
+        'title',
+        'slug'
+    ]
+    
+    inlines = [
+        OrganizerAccountInline,
+        AboutOrganizerInline
+    ]
+
+
+admin.site.register(Organizer, OrganizerAdmin)
