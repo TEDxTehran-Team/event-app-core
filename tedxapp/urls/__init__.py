f3a69687c2c798e81c0586734258393e7d4d51c2
Hasan Noori <xishma@gmail.com>
2020-04-01 12:33:06 +0430

    Updates base urls

--- tedxapp/urls/__init__.py
+++ tedxapp/urls/__init__.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib import admin
 
-urlpatterns = (
-    [
-        path("v1/", include("tedxapp.urls.v1")),
-    ]
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("v1/", include("tedxapp.urls.v1")),
+] + \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)s
