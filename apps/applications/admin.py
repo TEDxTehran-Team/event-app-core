a66c7df501f03a30c25981236ef3d86d27459a97
Hasan Noori <xishma@gmail.com>
2020-04-01 13:03:35 +0430

    Adds basic admin for applications

--- apps/applications/admin.py
+++ apps/applications/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Application, ApplicationToken
+
+
+class ApplicationTokenInline(admin.TabularInline):
+    model = ApplicationToken
+    extra = 0
+    readonly_fields = ['key']
+    can_delete = True
+
+
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'organizer'
+    ]
+    list_filter = []
+    readonly_fields = []
+    filter_horizontal = []
+    search_fields = [
+        'title',
+        'organizer__title',
+        'slug'
+    ]
+    exclude = []
+    inlines = [ApplicationTokenInline]
+
+
+class ApplicationTokenAdmin(admin.ModelAdmin):
+    list_display = [
+        'application',
+        'key',
+        'active'
+    ]
+    list_filter = ['active']
+    readonly_fields = [
+        'created_at',
+        'key'
+    ]
+    filter_horizontal = []
+    search_fields = [
+        'application__title',
+        'application__slug',
+        'key'
+    ]
+    exclude = []
+
+
+admin.site.register(Application, ApplicationAdmin)
+admin.site.register(ApplicationToken, ApplicationTokenAdmin)
