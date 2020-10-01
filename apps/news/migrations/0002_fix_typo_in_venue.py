b7d97236fd2cf929cda2d7235164a6d56f0baa5e
bardia <bardia.abhari13@gmail.com>
2020-10-01 13:47:37 +0330

    fix(migrations): conflict

--- apps/news/migrations/0002_fix_typo_in_venue.py
+++ apps/news/migrations/0002_fix_typo_in_venue.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('news', '0001_initial'),
+        ('news', '0002_auto_20200629_1332'),
     ]
 
     operations = [
