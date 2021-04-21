d196878d8dfa36aa089020ad9db71b85822e0c4d
M. Hosseyn Najafi <hosseynjf@icloud.com>
2021-04-21 12:40:48 +0430

    feat(auth): update the user model's fields

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as OriginalUserAdmin
 
 from apps.accounts.models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(OriginalUserAdmin):
+    ordering = ('phone',)
+
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,7 +1,42 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
+    USERNAME_FIELD = 'phone'
+
+    username = None
+
+    first_name = models.CharField(
+        _('first name'),
+        max_length=30,
+        blank=True,
+        null=True,
+    )
+
+    last_name = models.CharField(
+        _('last name'),
+        max_length=150,
+        blank=True,
+        null=True,
+    )
+
+    email = models.EmailField(
+        _('email address'),
+        blank=True,
+        null=True,
+    )
+
+    phone = models.CharField(
+        _('phone number'),
+        max_length=32,
+        unique=True,
+        validators=[
+            RegexValidator(r'^09\d{9}$'),
+        ],
+    )
+
     class Meta:
         db_table = 'auth_user'
